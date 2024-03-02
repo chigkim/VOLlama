2a4d12276b04e01b7caa9e8144da08fd92259c9a
chigkim <chigkim@users.noreply.github.com>
2024-03-01 23:11:56 -0500

    scripts to build app with console output for debug.

---
+++ build-debug.bat
@@ -0,0 +1,2 @@
+pip uninstall -y playwright
+pyinstaller --add-data "send.wav;." --add-data "receive.wav;." --add-data "default-parameters.json;." --add-data ".venv/Lib/site-packages/llama_index/core/_static/nltk_cache;llama_index/core/_static/nltk_cache" VOLlama.py
