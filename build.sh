a7f9626ed3dbd5cb8bfd28d5d88f89ddfdac4f2e
Streamlit <streamlit@localhost>
2025-04-08 10:53:42 -0400

    Remove Selenium.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm -rf __pycache__
 rm -rf build
 rm -rf dist
-pip uninstall -y playwright
+pip uninstall -y playwright selenium
 pyinstaller --noconsole --add-data="send.wav:." --add-data="receive.wav:." --add-data="default-parameters.json:." --add-data ".venv/Lib/python3.12/site-packages/llama_index/core/_static/nltk_cache:llama_index/core/_static/nltk_cache" VOLlama.py
